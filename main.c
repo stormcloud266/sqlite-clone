ba2af120bd77e1cf1fa8c4478e1714cd0e3303c7
Tawnee <allprt89@gmail.com>
2023-12-04 19:57:53 -0700

    add input buffer struct and function

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  printf("hello");
-  return 0;
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() { printf("db > "); }
+
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+    
+    return 0;
 }
